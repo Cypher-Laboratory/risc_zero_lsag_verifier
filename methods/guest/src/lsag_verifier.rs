9324ae280c10bc4e2f93ce532d1cab37b8811c83
Hussenet Thomas <Lejamon.c@protonmail.com>
2024-10-11 16:47:02 +0200

    update: find how to have the same hash output in rust and solidity

--- methods/guest/src/lsag_verifier.rs
+++ methods/guest/src/lsag_verifier.rs
@@ -1,8 +1,3 @@
-use core::str;
-use ethabi::{encode, Token};
-use k256::elliptic_curve::point::AffineCoordinates;
-use sha2::{Digest, Sha256};
-
 use crate::utils::scalar_from_hex::scalar_from_hex;
 use crate::utils::serialize_point::{deserialize_point, serialize_point};
 use crate::utils::serialize_ring::{deserialize_ring, serialize_ring};
@@ -10,8 +5,13 @@ use crate::utils::sha256::sha_256;
 use crate::utils::{hash_to_secp256k1::hash_to_secp256k1, hex_to_decimal::hex_to_decimal};
 use base64::engine::general_purpose;
 use base64::Engine;
+use core::str;
+use ethabi::ethereum_types::U256;
+use ethabi::{encode, Token};
+use k256::elliptic_curve::point::AffineCoordinates;
 use k256::{AffinePoint, Scalar};
 use serde::Deserialize;
+use sha2::{Digest, Sha256};
 
 // Define a struct that matches the structure of your JSON string
 #[derive(Deserialize, Debug)]
@@ -209,138 +209,41 @@ pub fn to_minimal_lsag_digest(
         ring: ring.to_vec(),
     };
     let encoded = abi_encode_minimal_lsag(&mini_lsag);
+    let mut result = vec![0u8; 32];
+    result[31] = 32u8;
+    result.extend_from_slice(&encoded);
+    //here result is the same as the abi.encode in solidity
+    let hex_string = format!("{}", hex::encode(&result));
+    dbg!(&hex_string);
+    //the issue come from the result of the sha256 function here.:qa
+
     let mut hasher = Sha256::new();
-    hasher.update(&encoded);
-    let result = hasher.finalize();
-    result.into()
+    hasher.update(&hex::decode(hex_string).expect("Error while decoding hex value"));
+    let hash_result = hasher.finalize();
+    let hash_bytes32: [u8; 32] = hash_result.into();
+    dbg!(hex::encode(hash_bytes32));
+    hash_result.into()
 }
 
 fn abi_encode_minimal_lsag(lsag: &MinimalLsag) -> Vec<u8> {
+    dbg!(lsag);
     let tokens = vec![
         Token::String(lsag.message.clone()),
         Token::String(lsag.linkability_flag.clone().unwrap_or_default()),
-        Token::FixedBytes(affine_point_to_bytes(&lsag.key_image)),
+        Token::Uint(affine_point_to_uint256(&lsag.key_image)), // Encode key_image as a uint256
         Token::Array(
             lsag.ring
                 .iter()
-                .map(|point| Token::FixedBytes(affine_point_to_bytes(point)))
+                .map(|point| Token::Uint(affine_point_to_uint256(&point))) // Each point encoded as uint256
                 .collect(),
         ),
     ];
-
     encode(&tokens)
 }
 
-fn affine_point_to_bytes(point: &AffinePoint) -> Vec<u8> {
-    // Similar to key_image_to_bytes, but for a single point in the ring
-    let mut bytes = Vec::new();
-    bytes.extend_from_slice(&point.x());
-    bytes
-}
-
-#[cfg(test)]
-mod tests {
-    use super::verify_lsag;
-    use crate::{
-        lsag_verifier::{compute_c, Params},
-        utils::{
-            scalar_from_hex::scalar_from_hex, scalar_to_string::scalar_to_string,
-            test_utils::get_ring,
-        },
-    };
-    use elliptic_curve::{sec1::FromEncodedPoint, PrimeField};
-    use k256::{AffinePoint, EncodedPoint, Scalar};
-
-    #[test]
-    fn test_compute_c() {
-        let ring = get_ring(&[
-            (
-                "10332262407579932743619774205115914274069865521774281655691935407979316086911",
-                "100548694955223641708987702795059132275163693243234524297947705729826773642827",
-            ),
-            (
-                "15164162595175125008547705889856181828932143716710538299042410382956573856362",
-                "20165396248642806335661137158563863822683438728408180285542980607824890485122",
-            ),
-            (
-                "23289579613515307249488379845935313471996837170244623503719929765426073488571",
-                "51508290999221377635014061085578700551081950582306096405012518980034910355762",
-            ),
-        ]);
-
-        let key_image = ring[0];
-
-        let params = Params {
-            index: 2,
-            previous_r: Scalar::from_u128(123),
-            previous_c: Scalar::from_u128(456),
-            previous_index: 1,
-            linkability_flag: Some("string".to_string()),
-            key_image,
-        };
-
-        let result = compute_c(
-            &ring,
-            "103322624075799327436197742051159142740698655217742816556919354079793160869111151641625951751250085477058898561818289321437167105382990424103829565738563622232895796135153072494883798459353134719968371702446235037199297654260734885712".to_string(),
-            "00000000000000000000000000000000000000000000000000000000075BCD15".to_string(), // hex for "123456789" (padded to 32 bytes)
-            &params,
-        );
-        let expected_result = "9417d5df80043f0a291210af035900c6863a560836fe23b25fc92b46fd87cb16";
-        assert_eq!(scalar_to_string(&result), expected_result);
-    }
-
-    #[test]
-    fn test_verify_lsag() {
-        // Define the points as strings
-        let points = [
-            (
-                "4051293998585674784991639592782214972820158391371785981004352359465450369227",
-                "88166831356626186178414913298033275054086243781277878360288998796587140930350",
-            ),
-            (
-                "10332262407579932743619774205115914274069865521774281655691935407979316086911",
-                "100548694955223641708987702795059132275163693243234524297947705729826773642827",
-            ),
-            (
-                "15164162595175125008547705889856181828932143716710538299042410382956573856362",
-                "20165396248642806335661137158563863822683438728408180285542980607824890485122",
-            ),
-            (
-                "23289579613515307249488379845935313471996837170244623503719929765426073488571",
-                "51508290999221377635014061085578700551081950582306096405012518980034910355762",
-            ),
-        ];
-        let ring = get_ring(&points);
-
-        let x = scalar_from_hex("191eb9f0636a5b1a87ed66cc00d5b3ffa35d4e04c4b21c8e48db987abb600b11");
-        let y = scalar_from_hex("2cdf899ff765f26abb272b8228ccc4b1f69192e614d9c0d44a52b78bb9af8774");
-        let key_image = AffinePoint::from_encoded_point(&EncodedPoint::from_affine_coordinates(
-            &x.unwrap().into(),
-            &y.unwrap().into(),
-            false,
-        ));
-        let c: k256::Scalar =
-            scalar_from_hex("86379b43861e950b5fa4b7571aff0c6004578e71280aaedb993833c9bde63c43")
-                .unwrap();
-
-        let result = verify_lsag(
-            &ring,
-            "message".to_string(),
-            c,
-            &[
-                scalar_from_hex("d6c1854eeb132d5886ac590c530a55a7fba3d92c4eb6896a728b0a61899ad902")
-                    .unwrap(),
-                scalar_from_hex("6a51d731b398036ed3b3b5cfd206407a35fd11faa2bbad1658bcf9f08b9c5fb8")
-                    .unwrap(),
-                scalar_from_hex("6a51d731b398036ed3b3b5cfd206407a35fd11faa2bbad1658bcf9f08b9c5fb8")
-                    .unwrap(),
-                scalar_from_hex("6a51d731b398036ed3b3b5cfd206407a35fd11faa2bbad1658bcf9f08b9c5fb8")
-                    .unwrap(),
-            ],
-            key_image.unwrap(),
-            Some("linkability flag".to_string()),
-        );
-
-        assert!(result);
-    }
+fn affine_point_to_uint256(point: &AffinePoint) -> U256 {
+    // Assuming `point.x()` returns a `[u8; 32]` representing the x-coordinate
+    let x_bytes = point.x().to_vec(); // Get x as [u8; 32]
+    dbg!(U256::from_big_endian(&x_bytes));
+    U256::from_big_endian(&x_bytes) // Convert the byte array to U256
 }
